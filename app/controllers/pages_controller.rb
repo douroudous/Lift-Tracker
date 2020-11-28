07c211eb58f38f188c48146b9b665dfff168b3e1
Dino Dourountoudakis <CDOUROUNTOUDAKIS@GMAIL.COM>
2020-11-28 12:47:07 -0500

    form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def home
-    @workouts = Workout.order(workout_date: :desc).last(2)
+    @workouts = Workout.order(workout_date: :desc).first(2)
   end
 end
--- app/helpers/workouts_helper.rb
+++ app/helpers/workouts_helper.rb
@@ -1,2 +1,21 @@
 module WorkoutsHelper
+  def workout_date_detail(date)
+    today_or_yesterday(date) || date.strftime("%a, %d %b")
+  end
+
+  def workout_date_day(date)
+    today_or_yesterday(date) || date.strftime("%a")
+  end
+
+  def workout_date_short(date)
+    date.strftime("%d %b")
+  end
+
+  def today_or_yesterday(date)
+    if date == Date.today
+      'Today'
+    elsif date == Date.yesterday
+      'Yesterday'
+    end
+  end
 end
