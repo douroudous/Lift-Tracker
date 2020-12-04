cc56de4c7f3a636b6d845e741c72303df902af29
Dino Dourountoudakis <CDOUROUNTOUDAKIS@GMAIL.COM>
2020-12-04 13:15:35 -0500

    remove-lift-sets

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root 'pages#home'
   resources :workouts
   resources :lift_workouts
-  resources :lift_sets
   resources :lifts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
