0e9406dfa3c060382a64b835bce8007364a14168
Dino Dourountoudakis <CDOUROUNTOUDAKIS@GMAIL.COM>
2020-12-18 14:32:26 -0500

    scaffold

---
+++ db/migrate/20201218192840_create_lift_sets.rb
@@ -0,0 +1,10 @@
+class CreateLiftSets < ActiveRecord::Migration[6.0]
+  def change
+    create_table :lift_sets do |t|
+      t.integer :rep_count, null: false
+      t.belongs_to :lift_workout, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
